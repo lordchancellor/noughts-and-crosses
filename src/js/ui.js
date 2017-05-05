86a0e73b3da91150ba64345fbf21c0cf6bfb2651
Christopher Ford <chris.ford@oneserve.co.uk>
2017-05-05 16:05:21 +0100

    Renamed icon to token across the board

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,6 @@
 export const ui = {
 	activateBoard: function activateBoard() {
-		document.getElementsByClassName('choose-icon')[0].style.display = 'none';
+		document.getElementsByClassName('choose-token')[0].style.display = 'none';
 		document.getElementsByClassName('board-outer')[0].style.display = 'flex';
 	}
 };
